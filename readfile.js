b16dd2200d279e2d50021a1d2165f92e6aa49696
Jason Metten <metten0@gmail.com>
2018-10-23 20:46:21 -0500

    string manipulation, mapping, object creation. TDD status red

--- readfile.js
+++ readfile.js
@@ -12,3 +12,53 @@ fs.readFile(file, "utf8", function(err, data) {
   if (err) throw err;
   return removeStopWordsFromArray(badWordz, data);
 });
+
+function isArray(test) {
+  return Array.isArray(test);
+}
+
+function isString(test) {
+  return typeof test === "string";
+}
+
+// lists final frequently-recurring words, sorted by frequency
+function splitByWords(text) {
+  // split string by spaces (including spaces, tabs, and newlines)
+  let wordsArray = text.split(/\s+/).map(function(word) {
+    return word.toLowerCase();
+    //new variable with properly split text, dropped to lowercase
+  });
+  return wordsArray;
+}
+
+function createWordMap(wordsArray) {
+  // create map for word counts
+  let wordsMap = {};
+
+  wordsArray.forEach(function(key) {
+    if (wordsMap.hasOwnProperty(key)) {
+      wordsMap[key]++;
+    } else {
+      wordsMap[key] = 1;
+    }
+  });
+  delete wordsMap[""];
+  return wordsMap;
+}
+
+function sortByCount(wordsMap) {
+  // sort by count in descending order
+  let finalWordsArray = [];
+  finalWordsArray = Object.keys(wordsMap).map(function(key) {
+    return {
+      name: key,
+      total: wordsMap[key]
+    };
+  });
+
+  finalWordsArray.sort(function(a, b) {
+    return b.total - a.total;
+  });
+
+  return finalWordsArray;
+}
