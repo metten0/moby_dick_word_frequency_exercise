93356b66070d0963c873a9607402d952c3904347
Jason Metten <metten0@gmail.com>
2018-10-23 20:19:49 -0500

    Brought in stop-words.txt, split into usable form

--- readfile.js
+++ readfile.js
@@ -1,3 +1,8 @@
 const fs = require("fs");
 const file = "mobydick.txt";
 const stopWords = "stop-words.txt";
+
+//read stop-words.txt file and assign it to a variable
+let badWordz = fs.readFileSync(stopWords, "utf8");
+//Split badWordz into a string, using the space as a separator, used 900 as an arbitrary limit of words
+badWordz = badWordz.split(" ", 900);
