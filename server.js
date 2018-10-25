04d9e0f297894cf0a8ef99bc0238f86fae7ff0e2
Jason Metten <metten0@gmail.com>
2018-10-25 13:32:46 -0500

    Refactors to accommodate deploy

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const path = require("path");
 const filtered_words = require("./filtered_words.json");
 const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
+app.use("/assets", express.static(path.resolve(__dirname, "assets")));
 
-app.get("/", (req, res) => {
+app.get("/api/words", (req, res) => {
   res.status(200).json(filtered_words);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(process.env.PORT || 5000, function() {
